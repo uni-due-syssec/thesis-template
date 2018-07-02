6ccfc22050d8bec1b2c64900f83ddf8492ed2178
odrai95 <draissioussama@googlemail.com>
2018-07-02 13:32:02 +0200

    docker images are now automatically deleted

--- compile.sh
+++ compile.sh
@@ -1,17 +1,22 @@
 #! /bin/sh 
 if [ -z "$1" ]; then
-    echo "usage: ./compile.sh file-to-compile (without file suffix)"
+    echo "usage: ./compile.sh file-to-compile"
     exit 1
 fi
+
+FILENAME=$(basename $1 .tex)
+
 
 # Builds a container with the files in the directory
 docker build -t syssec/tex -f Dockerfile .
 
 # Runs latexmk in the container
-docker run syssec/tex latexmk -f $1
+docker run syssec/tex latexmk -f $FILENAME
 
 # Copies the pdf into the directory
-docker cp $(docker ps -alq):/syssec-build/$1.pdf .
+docker cp $(docker ps -alq):/syssec-build/$FILENAME.pdf .
 
-# kills the container
+
+# kills and stops the container
 docker stop $(docker ps -alq)
+docker rmi --force $(docker images -qf "dangling=true")
