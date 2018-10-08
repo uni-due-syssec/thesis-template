af94ac72ecbe6f62749df1a4c29be8ba9b7b0b9a
odrai95 <draissioussama@googlemail.com>
2018-10-08 12:18:41 +0200

    gitlab is now on the same commit as github; added .gitignore; now thesis specific components are only loaded if the thesis class is loaded

--- compile.sh
+++ compile.sh
@@ -1,17 +1,22 @@
 #! /bin/sh 
 if [ -z "$1" ]; then
-    echo "usage: ./compile.sh file-to-compile (without file suffix)"
+    echo "usage: ./compile.sh file-to-compile"
     exit 1
 fi
+
+CONTAINER_TO_DELETE=$(docker images -qf "dangling=true")
+DOCKER_ID=$(docker ps -alq)
+FILENAME=$(basename $1 .tex)
 
 # Builds a container with the files in the directory
 docker build -t syssec/tex -f Dockerfile .
 
 # Runs latexmk in the container
-docker run syssec/tex latexmk -f $1
+docker run syssec/tex latexmk -f $FILENAME
 
 # Copies the pdf into the directory
-docker cp $(docker ps -alq):/syssec-build/$1.pdf .
+docker cp $DOCKER_ID:/syssec-build/$FILENAME.pdf .
 
-# kills the container
-docker stop $(docker ps -alq)
+# kills and stops the container
+docker stop $DOCKER_ID
+docker rmi --force $CONTAINER_TO_DELETE
